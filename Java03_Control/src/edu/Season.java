d30ddc03bae8584dad4f71db13e56d5fe37bc1cd
Daehan <114852681+SpaceXdaehan@users.noreply.github.com>
2023-06-26 17:56:20 +0900

    Add files via upload

---
+++ Java03_Control/src/edu/Season.java
@@ -0,0 +1,9 @@
+package edu.java.switch03;
+
+// Enum: 열거형 자료 타입 (enumeration type). 한정된 값(상수) 들만 갖는 데이터 타입.
+// 계절: (봄, 여름, 가을, 겨울) 식사시간: (아침, 점심, 저녁) 요일:
+// 자바에서 상수들은 전부 대문자 사용
+
+public enum Season {
+	SPRING, SUMMER, FALL, WINTER;
+}
