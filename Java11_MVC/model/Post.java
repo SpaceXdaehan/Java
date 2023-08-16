dc6d7bb8d5efe8406386f59c01325008f1d7dcd3
Daehan <114852681+SpaceXdaehan@users.noreply.github.com>
2023-08-16 09:17:24 +0900

    Add files via upload

---
+++ Java11_MVC/model/Post.java
@@ -0,0 +1,12 @@
+package edu.java.mvc.model;
+
+import java.time.LocalDateTime;
+
+public class Post {
+	private int pid;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdTime;
+    private LocalDateTime modifiedTime;
+}
