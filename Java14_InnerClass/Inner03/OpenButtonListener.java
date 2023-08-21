e0ea9ed5c96351c25378aa3bf3be32467cc48213
Daehan <114852681+SpaceXdaehan@users.noreply.github.com>
2023-08-21 09:20:02 +0900

    Add files via upload

---
+++ Java14_InnerClass/Inner03/OpenButtonListener.java
@@ -0,0 +1,12 @@
+package edu.java.inner03;
+
+import edu.java.inner03.Button.OnClickListener;
+
+public class OpenButtonListener implements OnClickListener{
+
+	@Override
+	public void onClick() {
+		System.out.println("파일 탐색기 실행... ");
+	}
+
+}
